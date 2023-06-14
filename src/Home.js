da650789bea226f0055e5d6e6fa00e4eedea6d0c
Ubinquitous <ubinquitous1@gmail.com>
2023-06-14 23:31:01 +0900

    FIX

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=977860049416-faqog6g742c0qqs4epucuu5biobgr3ph.apps.googleusercontent.com&redirect_uri=http://localhost:3000/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
+
+  return (
+    <div>
+      <a href={authUrl}>Google 로그인</a>
+    </div>
+  );
+};
+
+export default Home;
