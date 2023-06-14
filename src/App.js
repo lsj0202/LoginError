d63f7b1de84b3346740588369332603ee6a9553c
Sang jin Lee <w1004123@gmail.com>
2023-06-14 22:38:56 +0900

    login error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+    const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=977860049416-faqog6g742c0qqs4epucuu5biobgr3ph.apps.googleusercontent.com&redirect_uri=http://localhost:3000/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
+
+  useEffect(() => {
+    // const urlParams = new URLSearchParams(window.location.search);
+    const params = window.location.search
+    
+    const code = params.substring(params.indexOf('?code=')+6, params.indexOf('&'))
+
+    console.log(code);
+
+    if (code) {
+      const params = new URLSearchParams();
+      params.append('code', code);
+
+      const exchangeCodeForToken = async () => {
+        try {
+          const response = await axios.post(`http://woongbin-p-e.kr/login/oauth2/code/google?${params}`, {}, {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          });
+
+          const { access, refresh } = response.data.token;
+          localStorage.setItem('accessToken', access);
+          localStorage.setItem('refreshToken', refresh);
+
+          console.log('Access token', access);
+        } catch (error) {
+          console.log('Google 로그인 오류:', error);
+        }
+      };
+
+      exchangeCodeForToken();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href={authUrl}>Google 로그인</a>
     </div>
   );
-}
+};
 
 export default App;
